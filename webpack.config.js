16a486163dd3ea61718310b96f394c13aac1c91b
Kevin Malakoff <kevin@pgkyc.com>
2019-07-17 13:37:10 -0700

    Move to web pack again

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: `idvpn-client.${process.env.NODE_ENV === 'production' ? 'min.' : ''}js`,
+    library: 'idvpn',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.mjs$/,
+        exclude: /node_modules|web_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  },
+  externals: {
+    'oidc-client': {
+      commonjs: 'oidc-client',
+      commonjs2: 'oidc-client',
+      amd: 'oidc-client',
+      root: 'Oidc'
+    }
+  }
+};
